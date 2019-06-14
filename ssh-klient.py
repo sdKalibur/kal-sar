f799d3f128538954b9e681297f1f8e7f089a0f24
rhel <kaliburx@gmail.com>
2019-06-14 16:56:55 +0100

    Merge branch 'master' of https://github.com/sdKalibur/kal-sar
    # Conflicts:
    #	kal-sar.py

--- ssh-klient.py
+++ ssh-klient.py
@@ -1,1 +1,21 @@
 import paramiko
+
+ # |      client = SSHClient()
+ # |      client.load_system_host_keys()
+ # |      client.connect('ssh.example.com')
+ # |      stdin, stdout, stderr = client.exec_command('ls -l')
+
+def copy_file(hostname, port, username, password, src, dst):
+    client = paramiko.SSHClient()
+    client.load_host_keys('sample.csv')
+    print("connecting to %4 \n with username=%s....\n" %(hostname, username))
+    t = paramiko.Transport(hostname, port)
+    t.connect(username=rhel,password=rhel)
+    sftp = paramiko.SFTPClient.from_transport(t)
+    print("copying file:;;;; %s " %(src, dst))
+    sftp.put(src,dst)
+    sftp.close()
+    t.close()
+
+
+copy_file('localhost', '22', 'rhel', 'rhel', 'sampple.csv', '/tmp/')
