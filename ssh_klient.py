1264d5c53a0feb6742bce1e2d4f8b24923124647
rhel <kaliburx@gmail.com>
2019-06-27 16:35:10 +0100

    Merge branch 'master' of https://github.com/sdKalibur/kal-sar
    # Conflicts:
    #	kal-sar.py

--- ssh_klient.py
+++ ssh_klient.py
@@ -9,22 +9,14 @@ from kal_sar import get_sysstat_day as get_sar
 
 def ssh_konnector():
     parser = argparse.ArgumentParser(description='Ssh into a server')
-    parser.add_argument('hostname',  type=string, nargs='+', help='hostname must dns resolvable or IP address.')
-    parser.add_argument(''-m',--mode',help="enter performance matrix", default='cpu_load')
-    parser.add_argument('-u',''--username', help="Specify a user with ssh access to target host.", default="rhel")
-    parser.add_argument('-p',''--password', help="Eeenter a password for the user in use.", default="rhel")
-    # if len(sys.argv) < 4:
-    #     print("args missing")
-    #     sys.exit()
+    parser.add_argument('hostname',  type=str, nargs='+', help='hostname must dns resolvable or IP address.')
+    parser.add_argument('-m','--mode',help="enter performance matrix", default='cpu_load')
+    parser.add_argument('-u','--username', help="Specify a user with ssh access to target host.", default="rhel")
+    parser.add_argument('-p','--password', help="Enter a password for the user in use.", default="rhel")
 
-
-    # hostname = sys.argv[1]
     port = 22
-    # password = sys.argv[2]
-    # command = sys.argv[3]
 
-    # command = get_sar()
-
+    # command = get_sar
     # username = 'rhel'
     # password = 'rhel'
     # port = 22
@@ -60,4 +52,4 @@ def main():
     pass
 
 if __name__ == '__main__':
-    ssh_konnector(*args)
+    ssh_konnector()
