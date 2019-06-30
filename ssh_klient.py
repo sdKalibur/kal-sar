6717cd57f29d7a648ffef3263bc080d37416b4da
kaliburx <kaliburx@gmail.com>
2019-06-30 10:44:08 +0100

    get paramiko sshclient

--- ssh_klient.py
+++ ssh_klient.py
@@ -1,43 +1,35 @@
+#!/usr/bin/env python3g
 
-#!/usr/bin/env python3
-
-import paramiko,sys, subprocess
+import paramiko
+import sys, subprocess
 import argparse
 
 from kal_sar import get_sysstat_day as get_sar
 
 
+
+parser = argparse.ArgumentParser(description='Ssh into a server')
+parser.add_argument('hostname', help='hostname must dns resolvable or IP address.', default='localhost', type=str )
+parser.add_argument('-m', '--mode', help="enter performance matrix", default='cpu_load')
+parser.add_argument('-u', '--username', help="Specify a user with ssh access to target host.", default="rhel")
+parser.add_argument('-p', '--password', help="Enter a password for the user in use.", default="rhel")
+parser.add_argument('-P', '--port', help="Set the ssh port for this connection.", default=22, type=int)
+
+print("trying to setup connection: \n" + str(parser.parse_args()))
 def ssh_konnector():
-    parser = argparse.ArgumentParser(description='Ssh into a server')
-    parser.add_argument('hostname',  type=string, nargs='+', help='hostname must dns resolvable or IP address.')
-    parser.add_argument(''-m',--mode',help="enter performance matrix", default='cpu_load')
-    parser.add_argument('-u',''--username', help="Specify a user with ssh access to target host.", default="rhel")
-    parser.add_argument('-p',''--password', help="Eeenter a password for the user in use.", default="rhel")
-    # if len(sys.argv) < 4:
-    #     print("args missing")
-    #     sys.exit()
+    """ssh client connector"""
+    command == 'top'
 
+    # command == mode
 
-    # hostname = sys.argv[1]
-    port = 22
-    # password = sys.argv[2]
-    # command = sys.argv[3]
-
-    # command = get_sar()
-
-    # username = 'rhel'
-    # password = 'rhel'
-    # port = 22
-    print(hostname, username, password, command)
-    command = input("enter your command: ")
-    print("trying to setup connection")
+    client.connect(hostname, timeout='20')
     try:
         client = paramiko.SSHClient()
         client.load_host_keys('known_hosts')
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
         #assert isinstance(password, object)
-        client.connect(hostname, port=port, username=username, password=password)
+        client.connect(hostname, timeout=20)
         print("I wanna run: ", command)
         stdin, stdout, stderr = client.exec_command(command)
         print(stdout.read())
@@ -55,9 +47,10 @@ def ssh_konnector():
 
 # ssh_konnector(command='uptime')
 # ssh_konnector(hostname='kalibur-mce', command="sadf   -d   --  /var/log/sysstat/sa15     -s 12:00 -e 17:00   -q")
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    ssh_konnector(*args)
+#
+# def main():
+#     ssh_konnector()
+#
+#
+# if __name__ == '__main__':
+#      main()
--- test_argsy.py
+++ test_argsy.py
@@ -1,8 +1,17 @@
-def sum(*args):
-    sum = 0
-    for _ in args:
-        sum += _
-    print(sum)
-    return sum
+# def sum(*args):
+# #     sum = 0
+# #     for _ in args:
+# #         sum += _
+# #     print(sum)
+# #     return sum
+# #
+# # sum(1,2,3,4,5,6)
 
-sum(1,2,3,4,5,6)
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("testArg")
+
+print(parser.parse_args())
+args = parser.parse_args()
+print(args)
