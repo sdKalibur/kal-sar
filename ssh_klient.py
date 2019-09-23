c466d8dca017373d059f677a0b64d4986feffcec
kaliburx <kaliburx@gmail.com>
2019-09-24 00:59:10 +0100

    created logger and exception blocks

--- ssh_klient.py
+++ ssh_klient.py
@@ -1,33 +1,51 @@
 # !/usr/bin/env python3
 
-import paramiko, sys, subprocess
+import paramiko, logging, datetime , subprocess
 import argparse
 
-from kal_sar import get_sysstat_day as get_sar
+# Create loggeimer
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s" # datefmt=\"%m/%d/%Y %I:%M:%S %p %Z\" "
 
-
+logging.basicConfig(filename="ssh-klient.log",
+                    level = logging.DEBUG,
+                    format = LOG_FORMAT,
+                    datefmt =  "%m/%d/%Y %I:%M:%S %p %Z"
+                    )
+                   # datefmt = "%m/%d/%Y %I:%M:%S %p %Z")
+logger = logging.getLogger()
+logTime = datetime.datetime.today().ctime()
 def ssh_konnector(hostname, password, username, port, command):
     """ssh connection object"""
 # https://www.programcreek.com/python/example/11460/paramiko.AuthenticationException
     print("trying to setup connection")
     try:
         # Define connection object
+        # checks for unreachable or otherwise fake hosts
         client = paramiko.SSHClient()
         client.load_host_keys('known_hosts')
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         # Make connection
+
         client.connect(hostname=hostname, port=port, username=username, password=password, timeout=10, auth_timeout=10,
                        gss_deleg_creds=True, allow_agent=True, )
+        stdin, stdout, stderr = client.exec_command(command)
+        logger.info(logTime, stderr)
         # raise NoValidConnectionsError(errors)
         #   paramiko.ssh_exception.NoValidConnectionsError: [Errno None] Unable to connect to port 22 on 10.42.0.2
+    except paramiko.ssh_exception.NoValidConnectionsError as err:
+        logger.error(logTime, err)
+        print("Failed to establish a connection", err)
+        raise err
+    except paramiko.ssh_exception.AuthenticationException as err:
+        logger.error(logTime, err)
+        print("Auth failed", err)
+        raise err
 
-        # Excute command on successful connection
-        print("I wanna run: ", command)
-        stdin, stdout, stderr = client.exec_command(command)
+    except:
+        print("An error ocured!")
+    finally:
         print("Stdout says : \n:", stdout.read().decode('utf-8'))
         print("stderr says : \n:", stderr.read().decode('utf-8'))
-
-    finally:
         client.close()
 
     print("Closing connection now.")
